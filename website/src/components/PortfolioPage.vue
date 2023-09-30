b18cd23419e15e0b269de6a82a2fb774bca52cfd
Heroes Of Balkan <73647896+HeroesOfBalkan@users.noreply.github.com>
2023-09-30 20:52:39 +0200

    finl chngs

--- website/src/components/PortfolioPage.vue
+++ website/src/components/PortfolioPage.vue
@@ -4,13 +4,58 @@
 
 
 <template>
-    <main>
+<main>
+    <h1>reveal(previous_work, work_total_amount);</h1>
+    <h4>Note: A lot of old work has been lost due to hard disk malfunction</h4>
 
-    </main>
+    <h2>Software: (most of publicly available projects can be found on my <a href="https://github.com/HeroesOfBalkan" target="_blank">Github</a>)</h2>
+    <ul>
+        <li>
+            Programirko (preview available on <a href=""></a>Github)
+        </li>
+        <li>
+            <a href="https://psi-privatni-forum.vercel.app/#/" target="_blank">Private forum</a>
+        </li>
+        <li>
+            <a href="https://github.com/HeroesOfBalkan/Dancler" target="_blank">Dancler</a>
+        </li>
+        <li>
+            Task automation in Python for local TV station "PrugaTV"
+        </li>
+    </ul>
+
+    <h2>Graphics Design (most of publicly available projects can be found on <a href="https://www.youtube.com/@heroes-of-balkan" target="_blank">Youtube</a> or <a href="https://www.instagram.com/heroesofbalkan/?hl=en">Instagram</a>) (watch at your own risk)</h2>
+    <ul>
+        <li><img src="../assets/portfolio/kosmet-majica-with-spasimo-kim.png" alt="Shirt for @spasimo_kim on IG." width="200"></li>
+        <li><img src="../assets/portfolio/petrovic-print-a0-svetla1.svg" alt="Logo Commision" width="200"></li>
+        <li><img src="../assets/portfolio/ytbanner.png" alt="My Youtube Banner" width="200"></li>
+        <li><img src="../assets/portfolio/ytbanner-commision.png" alt="YT Banner Commision" width="200"></li>
+        <!-- <li> Broken ... :'(
+            <video width="200" controls>
+                <source src="../assets/portfolio/rafchallenge2021.avi" type="video/avi">
+                HTML5 `video` tag unsupported on this browser.
+            </video>
+        </li> -->
+        <!-- <li>
+            <video width="200" controls>
+                <source src="../assets/portfolio/madchallenge.mp4" type="video/mp4">
+                HTML5 `video` tag unsupported on this browser.
+            </video>
+        </li> -->
+    </ul>
+</main>
   
 </template>
 
 
 
 <style scoped>
+h1 {
+    font-family: monospace;
+    font-size: 3rem;
+}
+ul {
+    width: 50%;
+    margin: 0 auto;
+}
 </style>
